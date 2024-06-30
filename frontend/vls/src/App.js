d97495f8d5fdff5d709b126a667a53938a90e699
ShayLavi190 <112388395+ShayLavi190@users.noreply.github.com>
2024-06-30 12:25:44 +0300

    merge the previous project to the new one

--- frontend/vls/src/App.js
+++ frontend/vls/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing necessary dependencies
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import ForgotPassword from "./pages/Login/forgotpassword";
+import FirstPage from "./pages/Login/FirstPage";
+import Home from "./pages/Admin/Home";
+import Proceses from "./pages/Admin/Proceses";
+import Contact from "./pages/Admin/Contact";
+import Customers from "./pages/Admin/Customers";
+import Analytics from "./pages/Admin/Analytics";
 
+// Main App component
 function App() {
+  useEffect(() => {
+    // Dynamically create the viewport meta tag
+    const viewportMeta = document.createElement('meta');
+    viewportMeta.name = 'viewport';
+    viewportMeta.content = 'width=device-width, initial-scale=1.0';
+    
+    // Append the meta tag to the document head
+    document.head.appendChild(viewportMeta);
+
+    // Cleanup function to remove the added meta tag when component unmounts
+    return () => {
+      document.head.removeChild(viewportMeta);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Routes>
+            <Route path="/" element={<FirstPage />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            <Route path="/homeAdmin" element={<Home />} />
+            <Route path="/processAdmin" element={<Proceses />} />
+            <Route path="/contactAdmin" element={<Contact />} />
+            <Route path="/customersAdmin" element={<Customers />} />
+            <Route path="/analyticsAdmin" element={<Analytics />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
